0cd0eaabefc0a394bbeb1e1bb99bd8c73e7e0f4b
Michael CC <mciccoto@amazon.com>
2023-02-05 09:56:09 +1000

    Added dracula colour theme

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,12 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  dracula = {
+    config = { "\27LJ\2\na\0\0\3\0\6\0\n6\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\0016\0\3\0009\0\4\0'\2\5\0B\0\2\1K\0\1\0\24colorscheme dracula\bcmd\bvim\nsetup\fdracula\frequire\0" },
+    loaded = true,
+    path = "/Users/michaelciccotosto-camp/.local/share/nvim/site/pack/packer/start/dracula",
+    url = "https://github.com/Mofiqul/dracula.nvim"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/Users/michaelciccotosto-camp/.local/share/nvim/site/pack/packer/start/packer.nvim",
@@ -92,6 +98,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: dracula
+time([[Config for dracula]], true)
+try_loadstring("\27LJ\2\na\0\0\3\0\6\0\n6\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\0016\0\3\0009\0\4\0'\2\5\0B\0\2\1K\0\1\0\24colorscheme dracula\bcmd\bvim\nsetup\fdracula\frequire\0", "config", "dracula")
+time([[Config for dracula]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
