669c425929738bc921dfe3bfd67526f32fa167e6
Michael CC <mciccoto@amazon.com>
2023-02-05 17:41:28 +1000

    Changed key bindings for harpoon and telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,14 @@
 local builtin = require('telescope.builtin')
 
+-- For help see type ':h telescope.builtin*'
+-- These are the suggested remaps taken from the telescope git hub page
+-- https://github.com/nvim-telescope/telescope.nvim
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string( { search = vim.fn.input("Grep > ") } );
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
