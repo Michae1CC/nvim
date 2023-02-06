dc03ad17a1dd15b0cc29843847059fcf3f557b98
Michael CC <mciccoto@amazon.com>
2023-02-06 12:30:07 +1000

    Added changes to vim setting

--- lua/michael/set.lua
+++ lua/michael/set.lua
@@ -1,29 +1,46 @@
+-- line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- tabs and indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.autoindent = true
 
 vim.opt.smartindent = true
 
+-- line wrapping
 vim.opt.wrap = false
 
+-- editor history
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- text searching
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- appearance
 vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.colorcolumn = "80"
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 8 -- Always have 8 line at the bottom of the screen
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+-- backspace
+vim.opt.backspace = "indent,eol,start"
 
-vim.opt.colorcolumn = "80"
+-- split windows
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- update time, the length of Vim waits before you stop
+-- typing before it triggers it triggers the plugin governed by setting
+vim.opt.updatetime = 500
+
