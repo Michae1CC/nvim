9783853ebfd56277d10ade5c5c86644f98d1da40
Michael CC <mciccoto@amazon.com>
2023-02-06 13:34:31 +1000

    added Josean Martinez editing plugins

--- lua/michael/packer.lua
+++ lua/michael/packer.lua
@@ -38,6 +38,9 @@ return require('packer').startup(function(use)
 	use('theprimeagen/harpoon')
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
+	use('christoomey/vim-tmux-navigator')
+	use('tpope/vim-surround')
+	use('numToStr/Comment.nvim')
 
 	use {
 		'VonHeikemen/lsp-zero.nvim',
