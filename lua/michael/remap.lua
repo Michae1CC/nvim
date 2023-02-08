71b3ed6256271c18d9495fa1f4105e3e515bc201
Michael CC <mciccoto@amazon.com>
2023-02-08 21:34:08 +1000

    changes remaps

--- lua/michael/remap.lua
+++ lua/michael/remap.lua
@@ -1,12 +1,18 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move selected text up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep search find centered
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
+-- Use to make make the text being pasted over get sent to the void buffer
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
