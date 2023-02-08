3e65c88e89abb92f2600b7a438bb5fc6838afb66
Michael CC <mciccoto@amazon.com>
2023-02-08 12:14:42 +1000

    changed harpoon shortcuts

--- lua/michael/packer.lua
+++ lua/michael/packer.lua
@@ -1,13 +1,13 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-        vim.cmd.packadd('packer.nvim')
-        return true
-    end
-    return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd.packadd("packer.nvim")
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
@@ -15,82 +15,84 @@ local packer_bootstrap = ensure_packer() -- true if packer was just installed
 -- import packer safely
 local status, packer = pcall(require, "packer")
 if not status then
-    return
+	return
 end
 
-vim.cmd.packadd('packer.nvim')
+vim.cmd.packadd("packer.nvim")
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    -- color theme 
-    use 'Mofiqul/dracula.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+	-- color theme
+	use("Mofiqul/dracula.nvim")
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    -- autocomplete
-    use('hrsh7th/nvim-cmp')
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	-- autocomplete
+	use("hrsh7th/nvim-cmp")
 
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("nvim-treesitter/playground")
+	use("p00f/nvim-ts-rainbow")
+	use("theprimeagen/harpoon")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	use("christoomey/vim-tmux-navigator")
+	use("tpope/vim-surround")
+	-- lua functions that many plugins use
+	use("nvim-lua/plenary.nvim")
+	-- commenting with gc
+	use("numToStr/Comment.nvim")
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
+	-- vscode file icons
+	use("kyazdani42/nvim-web-devicons")
+	use("nvim-lualine/lualine.nvim")
 
-    use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
-    use('nvim-treesitter/playground')
-    use('p00f/nvim-ts-rainbow')
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use('christoomey/vim-tmux-navigator')
-    use('tpope/vim-surround')
-    -- lua functions that many plugins use
-    use("nvim-lua/plenary.nvim")
-    -- commenting with gc
-    use("numToStr/Comment.nvim")
-    -- file explorer
-    use("nvim-tree/nvim-tree.lua")
-    -- vscode file icons
-    use("kyazdani42/nvim-web-devicons")
-    use("nvim-lualine/lualine.nvim")
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
 
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
-    }
+	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
 
-    use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
-    use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-    use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+	-- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
 
-    -- formatting & linting
-    use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-    use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+	-- Taken from:https://github.com/windwp/nvim-autopairs
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 
-    -- Taken from:https://github.com/windwp/nvim-autopairs 
-    use {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
-    }
-
-    if packer_bootstrap then
-        require("packer").sync()
-    end
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
